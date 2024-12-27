bd7fb6ae7cbaebd5537604b43cb977dc883421a0
Hector T <42476070+UnPolinomio@users.noreply.github.com>
2024-12-27 14:16:34 -0600

    Add config files and template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fore(x, a, b) for (lli x = a, __lim__ = b; x < __lim__; ++x)
+#define all(x) begin(x), end(x)
+
+using lli = long long int;
+using vi = vector<lli>;
+using vb = vector<bool>;
+
+int main() {
+  ;
+  return 0;
+}
